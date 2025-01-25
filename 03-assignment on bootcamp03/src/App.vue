277ba644c0cad4faec350268eed2a441af8610ea
giuliXtor <giulia.tortorella@outlook.it>
2025-01-25 03:51:07 +0100

    Update App.vue
    I inserted the modification needed according to the exercises requirements.

--- 03-assignment on bootcamp03/src/App.vue
+++ 03-assignment on bootcamp03/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-    <div id="navBar" class="flexRow spaceBetween alignCenter">
+ 
+ <div id="navBar" class="flexRow spaceBetween alignCenter">
         <div class="flexRow">
             <i class="mdi mdi-food-fork-drink" style="font-size: 25px; padding-right: 10px"></i>
             <h3 style="margin:0">Giulia's Cafe</h3>
@@ -12,51 +13,128 @@
     </div>
     <div class="flexRow" style="height: calc(100vh - var(--navBarHeight));">
         <div id="sidebar" class="flexColumn">
-            <span class="mealTime" id="breakfast">Breakfast</span>
-            <span class="mealTime" id="lunch">Lunch</span>
+            <span class="mealTime" id="breakfast" @click="toggleBreakfast">Breakfast</span>
+            <span class="mealTime" id="lunch" @click="toggleLunch">Lunch</span>
             <span class="mealTime" id="dinner">Dinner</span>
+            <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+            <div class="flexRow emptyRow">
+            <!-- Empty row styled with custom classes -->
+            </div>
+            <p style="margin-left: 8px"> Total order: {{ total_order }} items </p>
         </div>
         <div id="main">
-            <div id="breakfastFoods" class="flexRow wrap">
+            <div v-if="breakfastShow" id="breakfastFoods" class="flexRow wrap">
                 <div class="menuItem">
                     <img src="./assets/breakfastImages/bagel.jpg" width="100%" height="133px">
                     <h4>Bagel</h4>
                     <p>Bagel with cream cheese</p>
+                    <!-- Button for Bagel -->
+                    <!-- Here we define what function will be called when button is clicked. -->
+                    <button @click="increment_bagels">Order</button>
+                    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+                    <p style="margin-left: 1px"> Number of Bagels: {{ count_bagels }} </p>
                 </div>
                 <div class="menuItem">
                     <img src="./assets/breakfastImages/omelet.jpg" width="100%">
                     <h4>Omelet</h4>
                     <p>4 egg omelet with cheese</p>
+                    <!-- Button for Omelet -->
+                    <!-- Here we define what function will be called when button is clicked. -->
+                    <button @click="increment_omelets">Order</button>
+                    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+                    <p style="margin-left: 1px"> Number of Omelets: {{ count_omelets }} </p>
                 </div>
                 <div class="menuItem">
                     <img src="./assets/breakfastImages/pancakes.jpg" width="100%">
                     <h4>Pancakes</h4>
                     <p>Pancakes with syrup</p>
+                    <!-- Button for pancakes -->
+                    <!-- Here we define what function will be called when button is clicked. -->
+                    <button @click="increment_pancakes">Order</button>
+                    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+                    <p style="margin-left: 1px"> Number of Pancakes: {{ count_pancakes }} </p>
                 </div>
             </div>
-            
-            <div id="lunchFoods" class="flexRow wrap">
+
+            <div v-if="lunchShow" id="lunchFoods" class="flexRow wrap">
                 <div class="menuItem">
                     <img src="./assets/lunchImages/soup.jpg" width="100%">
                     <h4>Soup</h4>
                     <p>Tomato</p>
+                    <!-- Button for Soup -->
+                    <!-- Here we define what function will be called when button is clicked. -->
+                    <button @click="increment_soups">Order</button>
+                    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+                    <p style="margin-left: 1px"> Number of Soups: {{ count_soups }} </p>
                 </div>
                 <div class="menuItem">
                     <img src="./assets/lunchImages/sandwhich.jpg" width="100%">
                     <h4>Sandwhich</h4>
                     <p>Chicken and Avocado</p>
+                    <!-- Button for sandwhich -->
+                    <!-- Here we define what function will be called when button is clicked. -->
+                    <button @click="increment_sandwhiches">Order</button>
+                    <!-- Now we print the value. The syntax to print variable is {{ variable_name }}. -->
+                    <p style="margin-left: 1px"> Number of Sandwhiches: {{ count_sandwhiches }} </p>
                 </div>
             </div>
-            
+
         </div>
     </div>
+
+
 </template>
 
 
+<!-- // Script is in some way equivalent to script.js. This is place
+  to define variables, methods and imports of other Vue compoents. -->
+  <script setup>
 
+  import { ref, computed } from 'vue';
+  
+  // Define variables and constants
+  var count_bagels = ref(0);
+  var count_omelets = ref(0);
+  var count_pancakes = ref(0);
+  var count_soups = ref(0);
+  var count_sandwhiches = ref(0);
+  
+  // Define functions
+  function increment_bagels() {
+    count_bagels.value++;
+  }
+  function increment_omelets() {
+    count_omelets.value++;
+  }
+  function increment_pancakes() {
+    count_pancakes.value++;
+  }
+  function increment_soups() {
+    count_soups.value++;
+  }
+  function increment_sandwhiches() {
+    count_sandwhiches.value++;
+  }
+  
+  let breakfastShow = ref(true)
+  let lunchShow = ref(true)
+  
+  function toggleBreakfast() {
+      breakfastShow.value = !breakfastShow.value
+      console.log("breakfastShow is" + breakfastShow.value)
+  }
+  
+  function toggleLunch() {
+      lunchShow.value = !lunchShow.value
+      console.log("luncShow is" + lunchShow.value)
+  }
+  // Computed property to calculate the total order count
+  const total_order = computed(() => {
+    return count_bagels.value + count_omelets.value + count_pancakes.value + count_soups.value + count_sandwhiches.value
+  })
+  </script>
 
-
-<style scoped>
+<style>
 :root {
     --navBarHeight: 30px;
     --sideBarHeight: 200px
@@ -90,7 +168,7 @@
 
 .menuItem {
     width: 200px;
-    height: 250px;
+    height: 235px;
     margin: 4px;
 }
 
@@ -102,7 +180,7 @@
 .box {
     height: 100px;
     width: 100px;
-    background-color:azure;
+    background-color: azure;
     border: 1px solid black;
     margin: 4px;
 }
@@ -135,12 +213,23 @@
     justify-content: flex-end;
 }
 
-body {
-    margin: 0px;
-}
-
 .mealTime {
     margin: 10px;
     color: white
+}
+
+.emptyRow {
+  height: 60px;  /* Adjust the height */
+  margin: 10px 0;  /* Optional margin to create space between sections */
+}
+
+h4 {
+
+    padding: 0;
+    margin: 0;
+}
+
+p {
+    margin: 0px;
 }
 </style>
